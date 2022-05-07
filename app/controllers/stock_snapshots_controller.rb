24f8aef8072eef97358786e71cb1aa5cc43e0a56
Spencer Michalski <semski13@gmail.com>
2022-05-07 22:33:41 +0000

    add view of individual stock_snapshot

--- app/controllers/stock_snapshots_controller.rb
+++ app/controllers/stock_snapshots_controller.rb
@@ -1,4 +1,9 @@
 class StockSnapshotsController < ApplicationController
   def index
+    @stock_snapshots = StockSnapshot.all
+  end
+
+  def show
+    @stock_snapshot = StockSnapshot.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "stock_snapshots#index"
   get "/addsnapshot", to: "stock_snapshots#index"
+  get "/stock_snapshots/:id", to: "stock_snapshots#show"
 end
