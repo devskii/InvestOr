6683167b6066c27c5e7190c25db74e14e11847fd
Spencer Michalski <semski13@gmail.com>
2022-05-08 00:09:17 +0000

    add view to create new stock_snapshot

--- app/controllers/stock_snapshots_controller.rb
+++ app/controllers/stock_snapshots_controller.rb
@@ -1,6 +1,19 @@
 class StockSnapshotsController < ApplicationController
+  def create
+    @stock_snapshot = StockSnapshot.new(ticker: "...")
+    if @stock_snapshot.save
+      redirect_to @stock_snapshot
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
   def index
     @stock_snapshots = StockSnapshot.all
+  end
+
+  def new
+    @stock_snapshot = StockSnapshot.new
   end
 
   def show
